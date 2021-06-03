068cfa954418479821aafe62e7ea4fa6de6b049a
Supritha Vemala <m1064218@mindtree.com>
2021-06-03 17:44:36 +0530

    deleted

--- Project/front-end/src/store/constants.ts
+++
@@ -1,4 +0,0 @@
-const LOGGED_IN='LOGGED_IN';
-const LOGGED_OUT='LOGGED_OUT';
-
-export {LOGGED_IN,LOGGED_OUT}
--- Project/front-end/src/store/store.ts
+++
@@ -1,6 +0,0 @@
-import {createStore} from "redux"
-import { userReducer } from "../reducers/reducers"
-
-const store=createStore( userReducer )
-
-export {store}
